30c0679e3ec84d126f29bb4ce8fb1d4467d1413e
Sergey Gatsenko <ya.stealth1072@yandex.ru>
2025-07-08 23:37:09 +0300

    fix:обновлена валидация

--- src/utils/validationRules.ts
+++ src/utils/validationRules.ts
@@ -4,6 +4,8 @@ export const validationRules: Record<string, RegExp> = {
     login: /^(?!\d+$)[a-zA-Z0-9_-]{3,20}$/,
     email: /^[a-zA-Z0-9._-]+@[a-zA-Z]+\.[a-zA-Z]{2,}$/,
     password: /^(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,40}$/,
+    oldPassword: /^(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,40}$/,
+    newPassword: /^(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,40}$/,
     phone: /^\+?\d{10,15}$/,
     message: /^(?!\s*$).+/,
 };
@@ -14,6 +16,8 @@ export const errorMessages: Record<string, string> = {
     login: 'От 3 до 20 символов, латиница, цифры, - или _',
     email: 'Неверный формат email',
     password: 'От 8 до 40 символов, хотя бы 1 заглавная и цифра',
+    oldPassword: 'От 8 до 40 символов, хотя бы 1 заглавная и цифра',
+    newPassword: 'От 8 до 40 символов, хотя бы 1 заглавная и цифра',
     phone: 'От 10 до 15 цифр, может начинаться с +',
     message: 'Сообщение не может быть пустым',
 };
