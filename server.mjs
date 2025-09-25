d90e5820f646f75983ea5bca6e855de5945619fb
Sergey Gatsenko <ya.stealth1072@yandex.ru>
2025-09-25 11:54:42 +0300

    fix(path)

--- server.mjs
+++ server.mjs
@@ -1,13 +1,17 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const { PORT = 3000 } = process.env;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.static('./dist'));
 
 app.get('/*"*"', function (req, res) {
     res.sendFile(path.join(__dirname, './dist/index.html'));
-    res.status(200);
 });
 
 app.listen(PORT, () => {
